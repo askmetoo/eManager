11ec963e7762b2fd9448475cfc2ec2a340acfe60
Fawad Qureshi <fawadhq@gmail.com>
2014-05-17 18:49:07 +0300

    updated solution packages, switched dependency injector StructureMap -> Ninject

--- eManager.Web/Global.asax.cs
+++ eManager.Web/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using eManager.Web.DAL.Repository;
+using Ninject;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -17,7 +19,49 @@ namespace eManager.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            RegisterDependencyResolver();
             //Database.SetInitializer(new DropCreateDatabaseAlways<eManager.Web.DAL.eManagerContext>());
+        }
+
+        private void RegisterDependencyResolver()
+        {
+            var kernel = new StandardKernel();
+
+            // container configuration goes here
+            kernel.Bind<IDepartmentRepository>().To<DepartmentRepository>();
+            kernel.Bind<IEmployeeRepository>().To<EmployeeRepository>();
+            kernel.Bind<IDependentRepository>().To<DependentRepository>();
+            kernel.Bind<IEventRepository>().To<EventRepository>();
+            kernel.Bind<IServiceRepository>().To<ServiceRepository>();
+
+            DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
+        }
+
+        public class NinjectDependencyResolver : IDependencyResolver
+        {
+            private readonly IKernel kernel;
+
+            public NinjectDependencyResolver(IKernel kernel)
+            {
+                this.kernel = kernel;
+            }
+
+            public object GetService(Type serviceType)
+            {
+                return this.kernel.TryGet(serviceType);
+            }
+
+            public IEnumerable<object> GetServices(Type serviceType)
+            {
+                try
+                {
+                    return this.kernel.GetAll(serviceType);
+                }
+                catch (Exception)
+                {
+                    return new List<object>();
+                }
+            }
         }
     }
 }
