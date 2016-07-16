7952b2ebee9bec0134addd8bf29f308d0f1fa0a4
Fawad Qureshi <fawadhq@gmail.com>
2016-07-16 21:51:31 +0300

    refactored DI configuration

--- eManager.Web/Global.asax.cs
+++ eManager.Web/Global.asax.cs
@@ -1,7 +1,4 @@
-﻿using eManager.Web.DAL.Repository;
-using Ninject;
-using System;
-using System.Collections.Generic;
+﻿using eManager.Web.Infrastructure;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -16,49 +13,8 @@ namespace eManager.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            RegisterDependencyResolver();
+            DependencyResolver.SetResolver(new NinjectDependencyResolver());
             //Database.SetInitializer(new DropCreateDatabaseAlways<eManager.Web.DAL.eManagerContext>());
-        }
-
-        private void RegisterDependencyResolver()
-        {
-            var kernel = new StandardKernel();
-
-            // container configuration goes here
-            kernel.Bind<IDepartmentRepository>().To<DepartmentRepository>();
-            kernel.Bind<IEmployeeRepository>().To<EmployeeRepository>();
-            kernel.Bind<IDependentRepository>().To<DependentRepository>();
-            kernel.Bind<IEventRepository>().To<EventRepository>();
-            kernel.Bind<IServiceRepository>().To<ServiceRepository>();
-
-            DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
-        }
-
-        public class NinjectDependencyResolver : IDependencyResolver
-        {
-            private readonly IKernel kernel;
-
-            public NinjectDependencyResolver(IKernel kernel)
-            {
-                this.kernel = kernel;
-            }
-
-            public object GetService(Type serviceType)
-            {
-                return this.kernel.TryGet(serviceType);
-            }
-
-            public IEnumerable<object> GetServices(Type serviceType)
-            {
-                try
-                {
-                    return this.kernel.GetAll(serviceType);
-                }
-                catch (Exception)
-                {
-                    return new List<object>();
-                }
-            }
         }
     }
 }
---
+++ eManager.Web/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,41 @@
+﻿using eManager.Web.DAL.Repository;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace eManager.Web.Infrastructure
+{
+    class NinjectDependencyResolver : IDependencyResolver
+    {
+        private readonly IKernel kernel;
+
+        public NinjectDependencyResolver()
+        {
+            this.kernel = new StandardKernel();
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return this.kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return this.kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IDepartmentRepository>().To<DepartmentRepository>();
+            kernel.Bind<IEmployeeRepository>().To<EmployeeRepository>();
+            kernel.Bind<IDependentRepository>().To<DependentRepository>();
+            kernel.Bind<IEventRepository>().To<EventRepository>();
+            kernel.Bind<IServiceRepository>().To<ServiceRepository>();
+        }
+    }
+}
